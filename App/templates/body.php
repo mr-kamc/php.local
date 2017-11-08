feef7b65e044dffcd4ae79ccc9f83c6166974498
Aleksey Kalugin <mr.kamc@gmail.com>
2017-11-08 13:50:40 +0300

    разработана страница article. Ссылки на каждую новость идут в формате  article.php?id=NNN, где NNN - номер новости

--- App/templates/body.php
+++ App/templates/body.php
@@ -14,6 +14,7 @@
             <h2><?php echo $article->caption; ?></h2>
             <p><?php echo $article->text; ?></p>
             <h3><?php echo $article->author; ?></h3>
+            <h4><a href="App/templates/article.php?id=<?php echo $article->id; ?>">открыть в отдельном окне</a></h4>
         </div>
     <?php endforeach; ?>
 
